2fdbfff6d5f1a091b6af258c8efcb5a0b97c58e4
vchau <van.chau@aalto.fi>
2019-08-28 19:53:48 +0300

    Add final corrected version of unicafe

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -6,32 +6,14 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good+neutral+bad
-  const average = (good-bad)/all
-  const positive = good/all*100
+  
   return (
     <div>
       <h1>Give feedback</h1>
       <Button handleClick={() => setGood(good+1)} text={'Good'}/>
       <Button handleClick={() => setNeutral(neutral+1)} text={'Neutral'}/>
       <Button handleClick={() => setBad(bad+1)} text={'Bad'}/>
-      <h1>Statistics</h1>
-      {
-        all !== 0 ? 
-        <div>
-          <table>
-            <thead>
-              <Statistics text={'Good'} value={good}/>
-              <Statistics text={'Neutral'} value={neutral}/>
-              <Statistics text={'Bad'} value={bad}/>
-              <Statistics text={'All'} value={all}/>
-              <Statistics text={'Average'} value={average}/>
-              <Statistics text={'Positive'} value={positive}/>
-            </thead>
-          </table>
-        </div>
-        : 'No feedback given'
-      }
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
--- part_1/unicafe/src/Statistics.js
+++ part_1/unicafe/src/Statistics.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import Statistic from './Statistic'
 
-const Statistics = ({text, value}) => {
+const Statistics = ({good, neutral, bad}) => {
+    const all = good+neutral+bad
+    const average = (good-bad)/all
+    const positive = good/all*100
     return (
-        <tr>
-        <td>{text}</td>
-        <td>{value} {text==='Positive' ? '%' : ''}</td> 
-        </tr>
+    <div>
+        <h1>Statistics</h1>
+        {
+            all !== 0 ? 
+            <div>
+            <table>
+                <thead>
+                <Statistic text={'Good'} value={good}/>
+                <Statistic text={'Neutral'} value={neutral}/>
+                <Statistic text={'Bad'} value={bad}/>
+                <Statistic text={'All'} value={all}/>
+                <Statistic text={'Average'} value={average}/>
+                <Statistic text={'Positive'} value={positive}/>
+                </thead>
+            </table>
+            </div>
+            : 'No feedback given'
+        }
+    </div>
     )
+    
 }
 
 export default Statistics
