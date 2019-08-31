3d40ae9a7d1e122c9843346be2178ce668c027df
vchau <van.chau@aalto.fi>
2019-08-31 14:42:58 +0300

    Add part 2.11

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './Filter';
 import ContactForm from './ContactForm'
 import Numbers from './Numbers';
+import axios from 'axios';
+
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456'},
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]) 
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searching, setSearching] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })   
+    }, [])
 
     const addPerson = (event) => {
         event.preventDefault()
