c79b3c87681e75efa3c81894bb433b0d9468a1bd
vanchau <van.chau@aalto.fi>
2020-08-31 10:42:14 +0300

    Added ex. 3.8

--- part_3/index.js
+++ part_3/index.js
@@ -4,7 +4,15 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", (req) => {
+  console.log(req.body);
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
